48abe8210ed944848cc660d2dae0600b07b30f34
gabrielramost <83712300+gabrielramost@users.noreply.github.com>
2023-03-25 17:37:43 -0500

    Update app.js

--- Lab_Estadistica_2/app.js
+++ Lab_Estadistica_2/app.js
@@ -1,5 +1,30 @@
-// Mantener la barra de navegación fija al hacer scroll
-window.addEventListener('scroll', function() {
-  const nav = document.querySelector('nav');
-  nav.classList.toggle('sticky', window.scrollY > 0);
+// Esperamos a que cargue el DOM
+document.addEventListener('DOMContentLoaded', function () {
+  // Obtenemos la barra de navegación
+  const navBar = document.querySelector('.navbar');
+
+  // Obtenemos todos los elementos de la barra de navegación
+  const navItems = navBar.querySelectorAll('.nav-item');
+
+  // Recorremos los elementos de la barra de navegación
+  navItems.forEach(function (item) {
+    // Añadimos un event listener al hacer clic en el elemento
+    item.addEventListener('click', function (event) {
+      // Prevenimos la acción por defecto del elemento
+      event.preventDefault();
+
+      // Obtenemos el href del elemento clicado
+      const target = this.getAttribute('href');
+
+      // Obtenemos la posición de la sección a la que se debe desplazar
+      const targetPosition = document.querySelector(target).offsetTop;
+
+      // Realizamos la animación de desplazamiento suave
+      window.scroll({
+        top: targetPosition - navBar.offsetHeight,
+        left: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
 });
