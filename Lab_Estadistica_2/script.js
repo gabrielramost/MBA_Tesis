0a5830af53c2d025ef97067868a2129dc67934cd
gabrielramost <83712300+gabrielramost@users.noreply.github.com>
2023-03-26 17:53:14 -0500

    Update script.js

--- Lab_Estadistica_2/script.js
+++ Lab_Estadistica_2/script.js
@@ -28,3 +28,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  nav.classList.toggle('active');
+});
